aa98e85557247219775f8a191bb23e7b26732619
DeVon Thompson <devon.thompson0@gmail.com>
2019-02-19 23:08:43 +0000

    accounts.py

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -3,11 +3,28 @@ class User(object):
     def __init__(self, first_name, last_name, email):
         pass
 
+        Class User(object):
+            def __init__(self, Tim_name, Smith_name, Tim@rmotr.com)
+            
+            self.Tim= name
+                self.first=Tim
+                self.last= Smith
+                self.email= Tim@rmotr.com
+                
+                self.posts = []
+            self.following = []
+
     def add_post(self, post):
         pass
+      post.set_user(self)
+        self.posts.append(post)
 
     def get_timeline(self):
         pass
 
+    total_posts = []
+    
     def follow(self, other):
         pass
+    
+        follow=[]
